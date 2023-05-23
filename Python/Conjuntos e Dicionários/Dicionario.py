b3767633a23cdff1f65eb4ec1c0da5af09a87ed3
Thatiana Napolitano <thatiananapolitano@gmail.com>
2023-05-23 19:25:37 -0300

    feat. dicionarios

---
+++ Python/Conjuntos e Dicionários/Dicionario.py
@@ -0,0 +1,25 @@
+# Dicionários são conjuntos, mas neles sempre terão uma chave acompanhada de um valor. (Key e value) 
+
+""" EXEMPLO:
+Quero cadastrar um jogo. O jogoNome, jogoDesenvolvedora, jogoAno, etc. Quanto mais informações sobre o mesmo item (JOGO) mais desorganizado fica.
+Ao invés de criar várias variáveis, eu posso criar um dicionário com as chaves e seus valores como no exemplo abaixo:
+"""
+
+jogo = {"Nome": "Super Mario", 
+        "Desenvolvedora": "Nintendo",
+        "Ano": 1990 }
+
+print(jogo["Nome"]) #Aqui posso acessar a chave específica do Nome do Jogo.
+print(jogo)
+
+#E se eu quiser adicionar mais uma chave com valor ao meu jogo. No caso a chave "gênero" com valor "aventura". 
+
+jogo["Genero"] = "Aventura"
+print(jogo)
+
+#Retorna apenas as chaves: 
+jogo.keys()
+#Retorna apenas os valores:
+jogo.values()
+#Devolve uma lista com tuplas (chave, valor)
+jogo.items() 
