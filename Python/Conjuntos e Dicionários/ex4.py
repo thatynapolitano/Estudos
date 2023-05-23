d04fd6654b0d8e725733308e96bf654681ff3c58
Thatiana Napolitano <thatiananapolitano@gmail.com>
2023-05-23 20:12:34 -0300

    feat: lambda function

--- Python/Conjuntos e Dicionários/ex4.py
+++ Python/Conjuntos e Dicionários/ex4.py
@@ -16,8 +16,8 @@ print(listaCidades)
 
 #Colocando em ordem crescente por latitude e longitude.
 
-listaNorteSul = sorted(listaCidades,key=lambda dicionario:dicionario["latitude"])  # A função sorted ordena a minha listaCidades. Porém o sorted não ordena da forma específica que queremos por si só. Por isso adicionamos key=lambda dicionario:dicionario["latitude"]. Lambda é uma forma de criar uma função direto na linha. Uma função dicionário que sai a latitude do dicionário. função com entrada e saída. A entrada é dicionario e a saída é latitude de dicionário. Por isso dicionario:dicionario["latitude"]. 
+listaSulNorte = sorted(listaCidades,key=lambda dicionario:dicionario["latitude"])  # A função sorted ordena a minha listaCidades. Porém o sorted não ordena da forma específica que queremos por si só. Por isso adicionamos key=lambda dicionario:dicionario["latitude"]. Lambda é uma forma de criar uma função de uma linha no formato entrada e saída. Uma função dicionário que sai a latitude do dicionário -> Ou seja função com entrada e saída. A entrada é dicionario e a saída é latitude de dicionário. Por isso dicionario:dicionario["latitude"]. 
 
-
+listaOesteLeste = sorted(listaCidades,key=lambda dicionario:dicionario["longitude"]) # A função sorted ordena a minha list
 
 
