839b5e1222e5a52e1f5c11d44922752f7c7b9d56
Thatiana Napolitano <thatiananapolitano@gmail.com>
2023-05-23 19:11:23 -0300

    feat: conjuntos

---
+++ Python/Conjuntos/Conjuntos.py
@@ -0,0 +1,18 @@
+#parenteses tuplas (10,20,30)
+#colchetes listas  [10,20,30]
+#chaves conjuntos {10,20,30}
+
+pares={2,4,6,8,10}
+impares= {1,3,5,7,9}
+primos = {2,3,5,7}
+compostos = {4,6,8,9}
+
+#unir conjuntos dos pares e impares
+#pares.union(impares)
+pares.intersection(primos)
+
+#verifica se o 7 está dentro dos primos
+7 in primos
+
+# verifica se o 9 não está dentro dos primos
+9 not in primos
