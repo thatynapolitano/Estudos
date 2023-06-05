ea6063a26826668f094b41857bc1a0653b09ced6
Thatiana Napolitano <thatiananapolitano@gmail.com>
2023-06-04 22:43:28 -0300

    feat. celsius to fahrenheit update

--- Javascript/Celsius-to-Fahrenheit.js
+++ Javascript/Celsius-to-Fahrenheit.js
@@ -8,17 +8,6 @@ F = C * 9/5 + 32
 
 */ 
 
-let fahrenheit = 34
-let celsius = (fahrenheit - 32) * 5/9
-
-function graus (fahrenheit) {
-    return celsius 
-}
-
-console.log(graus()); 
- 
-// Outro jeito 
-
 function transformDegree(degree) {
     const celsiusExists = degree.toUpperCase().includes("C")
     const fahrenheitExists = degree.toUpperCase().includes("F")
@@ -26,12 +15,25 @@ function transformDegree(degree) {
     if(!celsiusExists && !fahrenheitExists) {
         throw new Error("Grau nao identificado")
     }
+
+    // fluxo ideal, F para C
+    let updatedDegree = Number(degree.toUpperCase.replace("F", ""));  
+    let formula = (fahrenheit) => (fahrenheit - 32) * 5/9
+    let degreeSign = "C"
+    
+    if(celsiusExists) {
+        updatedDegree = Number(degree.toUpperCase.replace("C", ""));  
+        let formula = (celsius) => celsius * 9/5 + 32
+        let degreeSign = "F"
+    }
+
+    return formula(updatedDegree) + degreeSign
+
 }
 
 try {
-    transformDegree("50F")
+    console.log(transformDegree("50C"))
     transformDegree("50Z")
 } catch (error) {
     console.log(error.message)
 }
-
