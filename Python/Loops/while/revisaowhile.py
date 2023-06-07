c3e3d17bf5969f62d0b1ef14f8a5a6bc7b60888c
Thatiana Napolitano <thatiananapolitano@gmail.com>
2023-06-07 19:32:09 -0300

    feat: while revision

---
+++ Python/Loops/while/revisaowhile.py
@@ -0,0 +1,6 @@
+# Ex loop
+
+nome = input("Digite seu nome: ")
+
+for caracter in nome:
+    print(caracter,end=" ")   # end=" " para colocar na mesma linha separando por espaco. Se eu retirar o end=" " , o nome fica separado por caracter linha por linha
