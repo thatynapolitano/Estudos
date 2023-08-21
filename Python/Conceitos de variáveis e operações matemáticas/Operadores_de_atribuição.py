83568fd05909d8d6a4c24f6a17eb349636424cea
Thatiana Napolitano <thatiananapolitano@gmail.com>
2023-08-21 20:44:44 -0300

    feat: Operadores de atribuição

---
+++ Python/Conceitos de variáveis e operações matemáticas/Operadores_de_atribuição.py
@@ -0,0 +1,29 @@
+#Atribuição simples
+
+saldo = 100
+print(saldo)
+
+#Atribuição com adição
+saldo = 500
+saldo += 200
+print(saldo)
+
+#Atribuição com divisão 
+saldo = 500
+saldo /= 5
+print(saldo)
+
+saldo = 500
+saldo //= 5
+print(saldo)
+
+#Atribuição com modulo
+saldo = 500
+saldo %= 480
+print(saldo)
+
+#Atribuição com exponenciacao
+saldo = 80 
+saldo **= 2
+print(saldo)
+
