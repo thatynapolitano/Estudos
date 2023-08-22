a55dd8a3d0bd9309016c4995e5af562f9332d378
Thatiana Napolitano <thatiananapolitano@gmail.com>
2023-08-21 21:46:48 -0300

    "feat: update"

--- Python/Condicionais/revisaoIF.py
+++ Python/Condicionais/revisaoIF.py
@@ -1,4 +1,9 @@
-#Revisão IF
+#Revisão IF e ELSE
+# Para criar uma estrutura condicional com dois desvios, podemos utilizar as palavras if e else. Como sabemos a expressão lógica testada no if for verdadeira, então o bloco de código do ig será executado. Caso contrário o bloco de código do else será executado.
+
+# IF - ELIF - ELSE
+# Em alguns cenários queremos mais de dois desvios, para isso podemos utilizar a palavra reservada elif. O elif é composto por uma nova expressão lógica, que será testada e caso retorne verdadeiro o bloco de código do elif será executado. Não existe um número máximo de elifs que podemos utilizar, porém evite criar grandes estruturas condicionais, pois elas aumentam a complexidade do código.
+
 
 nome1 = input('Digite o primeiro nome: ')
 nome2 = input('Digite o segundo nome: ')
@@ -10,4 +15,24 @@ elif len(nome2) > len(nome1):
 else:
   print('Ambos são iguais')
 
+
+# Outro exemplo 
+
+conta_normal = True 
+conta_universitaria = False
+
+saldo = 2000
+saque = 500 
+cheque_especial = 450
+
+if conta_normal:
+  if saldo >= saque:
+    print("Saque realizado com sucesso!")
+  elif saque <= (saldo + cheque_especial):
+    print("Saque realizado com uso do cheque especial")
+elif conta_universitaria:
+  if saldo >= saque:
+    print("Saque realizado com sucesso!")
+  else: 
+    print("Saldo insuficiente!")
   
--- Python/Estruturas de Repetição/for in/for concept.py
+++ Python/Estruturas de Repetição/for in/for concept.py
@@ -1,4 +1,5 @@
 # FOR
+# O comando FOR é usado para percorrer um objeto iterável. Faz sentido usar quando sabemos o número exato de vezes que nosso bloco de código deve ser executado, ou quando queremos percorrer um objeto iterável.
 
 listaAlunos = ["mario", "luigi", "peach", "yoshi"]
 for aluno in listaAlunos: 
@@ -10,3 +11,12 @@ for i in range(1,11): # se eu colocar 10 no lugar do 11, a contagem vai até o n
     
 for i in range(10, 0, -1):
     print(i)
+
+# Outro exemplo:
+texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto: 
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+print()
