90cabedf26b7fc30b84ce6f10acd81c9aac4327f
Thatiana Napolitano <103349907+thatynapolitano@users.noreply.github.com>
2023-11-12 14:26:48 -0300

    Update extend.py

--- Python/Lists/Lists methods/extend.py
+++ Python/Lists/Lists methods/extend.py
@@ -7,3 +7,12 @@ print(linguagens)
 linguagens.extend(["java", "c#"])
 
 print(linguagens)
+
+#Outra forma de juntar uma lista com outra: 
+
+lista1 = [1,2,3,4,5] 
+lista2 = [6,7,8,9,10] 
+
+lista1.extend(lista2)
+
+print(lista1) 
