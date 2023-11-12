df4b242b23d6f9d7763a0ea6349cab368ebfebff
Thatiana Napolitano <103349907+thatynapolitano@users.noreply.github.com>
2023-11-12 14:22:24 -0300

    Update and rename append-insert2.py to append_for.py

--- Python/Lists/Lists methods/append_for.py
+++ Python/Lists/Lists methods/append_for.py
@@ -7,12 +7,5 @@ print (my_list)
 
 ## Mesma sequencia de numeros acima, mas em ordem inversa 
 
-my_list = [] # Criando uma lista vazia.
-
-for i in range(5):
-   my_list.append (i + 1)
-
-print (my_list)
-
-#Esse é o motivo de usar o método insert.
+=
 
