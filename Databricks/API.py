32218c5ffb78bb6472468979830ebb68f058c7bb
Thatiana Napolitano <thatiananapolitano@gmail.com>
2024-04-02 21:27:12 -0300

    adicionando& C:/Users/thaty/AppData/Local/Programs/Python/Python312/python.exe c:/Users/thaty/Desktop/Estudos/Databricks/API.py

--- Databricks/API.py
+++ Databricks/API.py
@@ -1,4 +1,5 @@
-import requests, csv 
+import requests
+import csv 
 
 api_key = "7355bc8e306b4feb8390955f3aad5108"
 
@@ -10,19 +11,28 @@ try:
     if response.status_code == 200:
         data = response.json()
         articles = data['articles']
+        status = data['status']
+        totalResults = data['totalResults']
 
         with open('news.csv', mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            writer.writerow(['Title', 'URL']) 
+            writer.writerow(['ID','Name', 'Author', 'Title', 'Description', 'URL', 'urlToImage', 'Status', 'TotalResults']) 
 
             for article in articles:
-                title = article['title']
-                url = article['url']
-                writer.writerow([title, url])  
-                
+                id = article.get('id', '') 
+                name = article.get('source', {}).get('name', '')
+                author = article.get('author', '')
+                title = article.get('title', '')
+                description = article.get('description', '')
+                url = article.get('url', '')
+                urlToImage = article.get('urlToImage', '')
+
+                writer.writerow([id,name,author,title,description,url,urlToImage, status, totalResults])  
+
+    else:
         print('Falha ao obter dados:', response.status_code)
-except Exception as e:
-    print('Erro:', e)
+except:
+    print('Erro')
 
 
 
