6e1a737fb0173b9af7d70160add0679288615389
Thatiana Napolitano <thatiananapolitano@gmail.com>
2024-04-04 00:22:53 -0300

    test

--- Databricks/API.py
+++ Databricks/API.py
@@ -1,10 +1,16 @@
-import requests
-import csv 
+# %%
+import requests,csv 
+from datetime import datetime, timedelta 
+# %%
 
+# %%
 api_key = "7355bc8e306b4feb8390955f3aad5108"
 
-url = f'https://newsapi.org/v2/everything?q=bitcoin&language=en&sortBy=popularity&apiKey={api_key}'
+#url = f'https://newsapi.org/v2/everything?q=bitcoin&language=en&sortBy=popularity&apiKey={api_key}'
+url = "https://newsapi.org/v2/everything?q=bitcoin&from=2024-01-01&=to=2024-04-02&language=en&sortBy=popularity&apiKey=7355bc8e306b4feb8390955f3aad5108"
+# %%
 
+# %%
 try:
     response = requests.get(url)
 
@@ -16,37 +22,25 @@ try:
 
         with open('news.csv', mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            writer.writerow(['ID','Name', 'Author', 'Title', 'Description', 'URL', 'urlToImage', 'Status', 'TotalResults']) 
+            writer.writerow(['ID','Name', 'Author', 'Title', 'Description', 'URL', 'urlToImage','publishedAt','content', 'Status', 'TotalResults']) 
 
             for article in articles:
-                id = article.get('id', '') 
+                id = article.get('source', {}).get('id', '') 
                 name = article.get('source', {}).get('name', '')
                 author = article.get('author', '')
                 title = article.get('title', '')
                 description = article.get('description', '')
                 url = article.get('url', '')
                 urlToImage = article.get('urlToImage', '')
+                publishedAt = article.get('publishedAt', '')
+                content = article.get('content', '')
 
-                writer.writerow([id,name,author,title,description,url,urlToImage, status, totalResults])  
+                writer.writerow([id, name, author, title, description, url, urlToImage, publishedAt, content, status, totalResults])  
 
     else:
         print('Falha ao obter dados:', response.status_code)
-except:
-    print('Erro')
-
-
-
-# try:
-#     response = requests.get(url)
-
-#     if response.status_code == 200:
-#         data = response.json()
-#         articles = data['articles']
-
-#         for article in articles:
-#             print(article['title'])
-#     else:
-#         print('Falha ao obter dados:', response.status_code)
-# except Exception:
-#     print('Erro:', Exception)
+except Exception as e:
+    print('Erro:', e)
+# %%
+    
 
