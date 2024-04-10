dec9712658c1e330db9b08f70ce2dd686d45f68d
Thatiana Napolitano <thatiananapolitano@gmail.com>
2024-04-10 18:41:22 -0300

    factorial in a while true

--- Python/Cálculo Fatorial/fatorial.py
+++ Python/Cálculo Fatorial/fatorial.py
@@ -5,13 +5,28 @@ def fatorial(N):
         return 1
     else:
         fat = 1 
+        print(fat)
         for i in range(1,N+1):
+            print(fat)
             fat *= i 
         return fat
-    
 
 print(fatorial(4))
 
 # Fatorial de 4 =  4*3*2*1
 
- 
+while True:
+
+    try: 
+        num = int(input("Informe o número que deseja o fatorial: "))
+        if num < 0:
+            print("O número precisa ser positivo!")
+            continue 
+        else: 
+            break
+    except:
+        print("O numero deve ser inteiro! Tente novamente")
+
+fat = fatorial(num)
+print(f"O fatorial de {num} é {fat}")
+    
