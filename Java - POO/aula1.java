a7b8ec1048d71cbe3a42e8e3b72a422fd73eb217
Thatiana Napolitano <thatiananapolitano@gmail.com>
2024-06-04 20:19:54 -0300

    adicionando comentários

--- Java - POO/aula1.java
+++ Java - POO/aula1.java
@@ -1,6 +1,13 @@
-public class Main
+// Java é uma linguagem de programação orientada a objetos desenvolvida na década de 90 por uma equipe de programadores chefiada por James Gosling, na empresa Sun Microsystems, que em 2008 foi adquirido pela empresa Oracle Corporation.
+
+
+// Primeiro Hello World em Java 
+// Toda classe começa com nome Maiusculo (Main)  - uma classe pode ser public (outras classes do sistema podem ter acesso a ela), private (Só quem está dentro da classe consegue acessa-la)
+
+public class Main 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) { // todo nome de metodo começa com letra minuscula.. (main) 
 		System.out.println("Hello World!");
 	}
 }
+
