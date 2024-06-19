68dc3a3cdb3c58e5e66dd06e66a1fe7af23ecb62
Thatiana Napolitano <thatiananapolitano@gmail.com>
2024-06-18 21:51:18 -0300

    array de carros. acessando informação de carro pelo index

--- Java - POO/Projeto2/src/App.java
+++ Java - POO/Projeto2/src/App.java
@@ -4,8 +4,6 @@ import java.text.NumberFormat;
 import java.util.Locale;
 import javax.swing.JOptionPane;
 
-import Classes.Vendedor;
-
 public class App {
     public static void main(String[] args) {
 
@@ -14,22 +12,42 @@ public class App {
         meuPrimeiroCarro.marca = "Nissan"; 
         meuPrimeiroCarro.ano = 2018;
         meuPrimeiroCarro.preco = 120.000f;
-
+        String apresentacao01 = meuPrimeiroCarro.modelo + " " + meuPrimeiroCarro.preco;
         
         Carro segundoCarro = new Carro();
         segundoCarro.modelo = "Ecosport";
         segundoCarro.marca = "Fusca";
         segundoCarro.ano = 2012; 
+        segundoCarro.preco = 140.000f;
+        String apresentacao02 = segundoCarro.modelo + " " + segundoCarro.preco;
 
         Carro terceiroCarro = new Carro();
         terceiroCarro.modelo = "Fusca";
         terceiroCarro.marca = "Fusca";
         terceiroCarro.ano = 1970;
+        terceiroCarro.preco = 90.000f;
+        String apresentacao03 = terceiroCarro.modelo + " " + terceiroCarro.preco;
 
+        Carro [] arrayDeCarros = new Carro[3];
+        arrayDeCarros[0] = meuPrimeiroCarro;
+        arrayDeCarros[1] = segundoCarro;
+        arrayDeCarros[2] = terceiroCarro;
+
+        for (int i = 0; i < arrayDeCarros.length; i++) {
+
+            String saida = arrayDeCarros[i].modelo + " R$" + arrayDeCarros[i].preco;
+
+            JOptionPane.showMessageDialog(null, saida);
+        }
+
+        // Para formatar o preço do carro para R$ 
         Locale locale = new Locale("pt", "BR"); 
         NumberFormat formatadorDeNumeros = NumberFormat.getCurrencyInstance(locale);
         String precoFormatado = formatadorDeNumeros.format(meuPrimeiroCarro.preco);
 
         JOptionPane.showMessageDialog(null, precoFormatado); // Só aceita 1 argumento além do null.
+        JOptionPane.showMessageDialog(null, apresentacao01);
+        JOptionPane.showMessageDialog(null, apresentacao02);
+        JOptionPane.showMessageDialog(null, apresentacao03); 
     }
 }
--- Java - POO/Projeto2/src/Carro.java
+++ Java - POO/Projeto2/src/Carro.java
@@ -7,3 +7,4 @@ public class Carro {
     public float preco;
     
 }
+ 
