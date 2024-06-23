dfb0f1e14fd43d4f5caefcf6fb4cb9ac70bf47ea
Thatiana Napolitano <thatiananapolitano@gmail.com>
2024-06-23 00:16:38 -0300

    método de preço com desconto e dados

--- Java - POO/Projeto3/App.java
+++ Java - POO/Projeto3/App.java
@@ -16,33 +16,23 @@ public class App {
         x.nome = "Arroz";
         x.regiao = "Brasil";
         x.preco = 7.5f; 
-
+        
         Comida[] arrayComidas = {new Comida(), new Comida()};
+
         arrayComidas[0].nome = "Dolly Guaraná";
         arrayComidas[0].regiao = "Brasil";
         arrayComidas[0].preco = 20f;
 
         arrayComidas[1].nome = "Farofa";
         arrayComidas[1].regiao = "Colômbia";
-        arrayComidas[1].preco = 10f; // grama
+        arrayComidas[1].preco = 10f;
 
-        String saida = arrayComidas[0].nome + " (" + arrayComidas[0].regiao + ") R$ " + 
-        arrayComidas[0].preco;
+        for (int i = 0; i < arrayComidas.length; i++) {
+            JOptionPane.showMessageDialog(null, arrayComidas[i].getDados());
 
-        JOptionPane.showMessageDialog(null, saida);
+            float desconto = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor do desconto"));
 
-        String saida2 = arrayComidas[1].nome + " (" + arrayComidas[1].regiao + ") R$ " + 
-        arrayComidas[1].preco;
-
-        JOptionPane.showMessageDialog(null, saida2);
-
-        // Para facilitar a nossa vida, podemos utilizar uma estrutura de repetição para passar pelos elementos do array
-
-        for (int i = 0; i < arrayComidas.length; i ++) {
-            String saida3 = arrayComidas[i].nome + " (" + arrayComidas[i].regiao + ") R$ " + 
-            arrayComidas[i].preco;
-    
-            JOptionPane.showMessageDialog(null, saida3);
+            JOptionPane.showMessageDialog(null, arrayComidas[i].getDadosComDesconto(desconto));
         }
     }
 }
--- Java - POO/Projeto3/Comida.java
+++ Java - POO/Projeto3/Comida.java
@@ -4,4 +4,19 @@ public class Comida {
     public String nome;
     public String regiao;
     public float preco;
+
+    public float getPrecoComDesconto(float desconto){
+        float valorComDesconto = preco * (1 - desconto);
+        return valorComDesconto;
+    }
+
+    // Com esse método consigo retornar os dados
+    public String getDados() {
+        return nome + " (" + regiao + ") R$ " + preco; 
+    }
+    
+    public String getDadosComDesconto(float desconto) {
+        return nome + " (" + regiao + ") R$ " + getPrecoComDesconto(desconto); 
+    }
+    
 }
