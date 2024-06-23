0a9651fe17927883e3dca8b37eece3bab56a78cc
Thatiana Napolitano <thatiananapolitano@gmail.com>
2024-06-22 23:21:19 -0300

    feature: loop for - array de comidas

--- Java - POO/Projeto3/App.java
+++ Java - POO/Projeto3/App.java
@@ -1,6 +1,6 @@
 package Projeto3;
 
-import javax.swing.JOptionPanel;
+import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
@@ -21,6 +21,7 @@ public class App {
         arrayComidas[0].nome = "Dolly Guaraná";
         arrayComidas[0].regiao = "Brasil";
         arrayComidas[0].preco = 20f;
+
         arrayComidas[1].nome = "Farofa";
         arrayComidas[1].regiao = "Colômbia";
         arrayComidas[1].preco = 10f; // grama
@@ -29,5 +30,19 @@ public class App {
         arrayComidas[0].preco;
 
         JOptionPane.showMessageDialog(null, saida);
+
+        String saida2 = arrayComidas[1].nome + " (" + arrayComidas[1].regiao + ") R$ " + 
+        arrayComidas[1].preco;
+
+        JOptionPane.showMessageDialog(null, saida2);
+
+        // Para facilitar a nossa vida, podemos utilizar uma estrutura de repetição para passar pelos elementos do array
+
+        for (int i = 0; i < arrayComidas.length; i ++) {
+            String saida3 = arrayComidas[i].nome + " (" + arrayComidas[i].regiao + ") R$ " + 
+            arrayComidas[i].preco;
+    
+            JOptionPane.showMessageDialog(null, saida3);
+        }
     }
 }
